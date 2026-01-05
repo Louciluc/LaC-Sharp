0066c043f79e862d972e9ae80074551bc45c41bf
Louci <lucasbuschendorf22102000@gmail.com>
2026-01-05 18:30:11 +0100

    Erster Versuch, aallerdings kann man keine generischen Typen addieren, weil + auch undefined sein kann.

---
+++ Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Security;
+using System.Numerics;
+
+namespace LaC_Sharp
+{
+	public bool hey()
+	{
+
+	}
+	public class LazyType<T>(Func<T> value) {
+		public Func<T> Value = value;
+
+		static public T operator +(LazyType<T> a, LazyType<T> b)
+		{
+			T o;
+			T p;
+			T l = o + p;
+			return a.Value() + b.Value();
+		}
+
+	}
+
+	public class LazyList<T>
+	{
+		public LazyType<T> 
+	}
+}
